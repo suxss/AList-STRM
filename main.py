3400402ccf964f0f3a064334edc8ab0a506374c3
suxss <82267359+suxss@users.noreply.github.com>
2024-07-13 16:09:26 +0800

    优化部分代码

--- main.py
+++ main.py
@@ -1,108 +1,91 @@
+# -*- coding: utf-8 -*-
+
+import os
+import time
+from typing import TypedDict, Generator, Any
+
 from webdav3.client import Client
-import os,time,requests
-
-def list_files(webdav_url, username, password):
-	# 创建WebDAV客户端
-	options = {
-		'webdav_hostname': webdav_url,
-		'webdav_login': username,
-		'webdav_password': password
-	}
-	
-
-	client = Client(options)
-	mulu=[]
-	wenjian=[]
-	q=1
-	while q<15:
-		try:		   
-		   # 获取WebDAV服务器上的文件列表
-			files = client.list()
-		except:
-			q+=1
-			print('连接失败，1秒后重试...')
-			time.sleep(1)
-		else:
-			if q>1:
-				print('重连成功...')
-			break
-
-	for file in files[1:]:
-		if file[-1]=='/':
-			mulu.append(file)
-		else:
-			wenjian.append(file)
-	return mulu,wenjian
+from webdav3.exceptions import *
 
 
-# 输入WebDAV地址、用户名和密码
-webdav_url = 'yourdomain:port/dav/guest/阿里云盘alist/电影/'#alist webdav 地址
-save_mulu='Z:/OpenWrt/mnt/mmcblk2p4/TV/电影/'#输出路径
-username = 'yourusername'#用户名
-password = 'yourpassword'#密码
+class File(TypedDict):
+    dir: str
+    name: str
+    ext: str
 
-# 调用函数获取文件列表并保存到本地
 
-l_0=list_files(webdav_url, username, password)[0]
-l_1=list_files(webdav_url, username, password)[1]
+class StrmGenerator:
+    def __init__(self, webdav_url: str, username: str, password: str, save_dir: str, remote_path: str,
+                 retry_times: int = 3):
+        self.options = {
+            'webdav_hostname': webdav_url,
+            'webdav_login': username,
+            'webdav_password': password
+        }
+        self.client = Client(self.options)
+        self.save_dir = save_dir
+        self.remote_path = remote_path
+        q = 0
+        while q < retry_times:
+            try:
+                if not self.client.check(remote_path):
+                    raise ConnectionException("连接失败")
+                break
+            except ConnectionException:
+                q += 1
+        else:
+            raise ConnectionException("多次重连失败")
 
-url_1=[webdav_url]
-url_2=[]
-url_3=[]
-wenjian_1=[webdav_url + str(k) for k in l_1]
-wenjian_2=[]
-wenjian_3=[]
-if l_0 !=[]:
-	for u in l_0:
-		url_2.append(webdav_url+u)
-		wenjian_2+=[webdav_url+u+str(i) for i in list_files(webdav_url+u, username, password)[1]]
-	for x in url_2:
-		l_x=list_files(x, username, password)[0]
-		if l_x !=[]:
-			for y in l_x:
-				url_3.append(x+y)
-				wenjian_3+=[x+y+str(j) for j in list_files(x+y, username, password)[1]]
-url=url_1+url_2+url_3
-wenjian_all=wenjian_1+wenjian_2+wenjian_3
-#print(url)
-# for a in url:
-	# os.makedirs(os.path.dirname(a.replace(webdav_url,'')),exist_ok=True)
+    def list_files(self):
+        def get_files(dir: str = ''):
+            for file in self.client.list(dir)[1:]:
+                if file.endswith('/'):
+                    for child_file in get_files(dir + file):  # type: File
+                        yield child_file
+                else:
+                    t = file.split('.')
+                    name, ext = t[0], t[-1]
+                    if len(t) == 1:
+                        ext = ''
+                    else:
+                        name = '.'.join(t[:-1])
+                    yield File(dir=dir, name=name, ext=ext.lower())
 
-for b in wenjian_all:
-	if b[-3:].upper() in ['MP4','MKV','FLV','AVI']:
-		if not os.path.exists(save_mulu+b.replace(webdav_url,'')[:-3]+'strm' ):
-			print('正在处理：'+b.replace(webdav_url,''))
-			try:
-				os.makedirs(os.path.dirname(save_mulu+b.replace(webdav_url,'')[:-3]+'strm'),exist_ok=True)
-				with open(save_mulu+b.replace(webdav_url,'')[:-3]+'strm',"w",encoding='utf-8') as f:
-					f.write(b.replace('/dav','/d'))
-			except:
-				try:
-					os.makedirs(os.path.dirname(save_mulu+b.replace(webdav_url,'').replace('：','.')[:-3]+'strm'),exist_ok=True)
-					with open(save_mulu+b.replace(webdav_url,'').replace('：','.')[:-3]+'strm',"w",encoding='utf-8') as f:
-						f.write(b.replace('/dav','/d'))
-				except:
-					print(b.replace(webdav_url,'')+'处理失败，文件名包含特殊符号，建议重命名！')
-	elif b[-3:].upper() in ['ASS','SRT','SSA']:
-		if not os.path.exists(save_mulu+b.replace(webdav_url,'') ):
-			p=1
-			while p<10:
-				try:
-					print('正在下载：'+save_mulu+b.replace(webdav_url,''))
-					r=requests.get(b.replace('/dav','/d'))
-					os.makedirs(os.path.dirname(save_mulu+b.replace(webdav_url,'')),exist_ok=True)
-					with open (save_mulu+b.replace(webdav_url,''),'wb')as f:
-						f.write(r.content)
-						f.close
-					#wget.download(b.replace('/dav','/d'),save_mulu+b.replace(webdav_url,''))
-				except:
-					p+=1
-					print('下载失败，1秒后重试...')
-					time.sleep(1)
-				else:
-					if p>1:
-						print('重新下载成功！')
-					break
-				
-print('处理完毕！')
-input('按任意键退出...')
+        for file in get_files(self.remote_path):
+            yield file
+
+    def generate_strm_by_files(self, webdav_files: Generator[File, Any, None], download_ext: tuple[str],
+                               strm_ext: tuple[str]):
+        for file in webdav_files:
+            absolute_path = os.path.normpath(f'{file["dir"]}{file["name"]}.{file["ext"]}').replace('\\', '/')
+            relative_path = os.path.relpath(absolute_path, self.remote_path)
+            save_path = os.path.join(self.save_dir, relative_path).replace('\\', '/')
+            save_dir = os.path.dirname(save_path)
+            if not os.path.exists(save_dir):
+                os.makedirs(save_dir)
+            if file['ext'] in download_ext:
+                if os.path.exists(save_path):
+                    continue
+                self.client.download_sync(absolute_path, save_path)
+            elif file['ext'] in strm_ext:
+                if os.path.exists(os.path.join(save_dir, f'{file["name"]}.strm')):
+                    continue
+                with open(os.path.join(save_dir, f'{file["name"]}.strm'), 'w',
+                          encoding='utf-8') as f:
+                    f.write(f'{self.options['webdav_hostname']}{absolute_path.replace('/dav', '/d', 1)}')
+            time.sleep(0.1)
+
+    def generate(self, download_ext: tuple[str] = ('jpg', 'png', 'webm', 'gif', 'srt', 'ass', 'ssa', 'nfo'),
+                 strm_ext: tuple[str] = ('mp4', 'mkv', 'flv', 'avi')):
+        self.generate_strm_by_files(self.list_files(), download_ext, strm_ext)
+
+
+if __name__ == '__main__':
+    webdav_url = 'yourdomain:port'  # alist 地址
+    remote_path = '/dav/path/'
+    save_dir = 'Z:/OpenWrt/mnt/mmcblk2p4/TV/电影/'  # 输出路径
+    username = 'yourusername'  # 用户名
+    password = 'yourpassword'  # 密码
+    strm_generator = StrmGenerator(webdav_url=webdav_url, remote_path=remote_path, username=username, password=password,
+                                   save_dir=save_dir)
+    strm_generator.generate()
