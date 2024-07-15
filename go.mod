ce7096e252caf5303f4613eccea667444fe123d7
suxss <82267359+suxss@users.noreply.github.com>
2024-07-15 20:37:44 +0800

    add code to the new branch

---
+++ go.mod
@@ -0,0 +1,7 @@
+module AList-STRM
+
+go 1.22
+
+require github.com/studio-b12/gowebdav v0.9.0
+
+require golang.org/x/net v0.27.0 // indirect
---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/studio-b12/gowebdav"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
+	"sort"
+	"strings"
+	"sync"
+)
+
+var (
+	host         = flag.String("h", "http://localhost:5244/dav", "AList webdav 地址")
+	user         = flag.String("u", "admin", "AList webdav 用户名")
+	password     = flag.String("p", "12345", "AList webdav 密码")
+	remoteDir    = flag.String("r", "", "AList directory")
+	localDir     = flag.String("l", ".", "Local directory")
+	downloadExts = flag.String("d", ".jpg,.jpeg,.png,.gif,.nfo,.srt,.ass,.ssa", "直接下载的文件后缀名(以逗号分隔)")
+	strmExts     = flag.String("s", ".mp4,.avi,.mkv,.flv", "生成strm的文件后缀名(以逗号分隔)")
+)
+
+func main() {
+	flag.Parse()
+	downloadExt := strings.Split(*downloadExts, ",")
+	strmExt := strings.Split(*strmExts, ",")
+	err := generate(*host, *user, *password, *remoteDir, *localDir, downloadExt, strmExt)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("生成完毕")
+}
+
+func generate(host string, user string, password string, remoteDir string, localDir string, downloadExt []string, strmExt []string) error {
+	client := gowebdav.NewClient(host, user, password)
+
+	if err := client.Connect(); err != nil {
+		return err
+	}
+	return walk(client, remoteDir, func(fi os.FileInfo, path string) error {
+		return parse(host, client, fi.Name(), path, localDir, downloadExt, strmExt)
+	})
+}
+
+func walk(client *gowebdav.Client, path string, callback func(fi os.FileInfo, path string) error, goMaxNum ...int) error {
+	goNum := 10
+	if len(goMaxNum) > 0 {
+		goNum = goMaxNum[0]
+	}
+	wg := new(sync.WaitGroup)
+	once := new(sync.Once)
+	tokens := make(chan struct{}, goNum)
+	files, err := client.ReadDir(path)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		tokens <- struct{}{}
+		wg.Add(1)
+		go func(file os.FileInfo) {
+			defer func() {
+				wg.Done()
+				<-tokens
+			}()
+			if file.IsDir() {
+				if e := walk(client, path+"/"+file.Name(), callback, goNum); e != nil {
+					once.Do(func() {
+						fmt.Println(e)
+						err = e
+					})
+				}
+			} else {
+				if e := callback(file, path); e != nil {
+					once.Do(func() {
+						fmt.Println(e)
+						err = e
+					})
+				}
+			}
+		}(file)
+	}
+	wg.Wait()
+	return nil
+}
+
+func downloadFile(client *gowebdav.Client, remotePath string, localDir string) error {
+	reader, err := client.ReadStream(remotePath)
+	if err != nil {
+		return err
+	}
+	if _, err := os.Stat(localDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(localDir, 0777); err != nil { //os.ModePerm
+			return err
+		}
+	}
+	fileName := path.Base(remotePath)
+	file, err := os.Create(localDir + "/" + fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, reader)
+	return err
+}
+
+func generateSTRM(host string, remotePath string, localDir string, fileName string) error {
+	if _, err := os.Stat(localDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(localDir, 0777); err != nil { //os.ModePerm
+			return err
+		}
+	}
+	file, err := os.Create(localDir + "/" + fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(strings.Replace(host, "/dav", "/d", 1) + "/" + remotePath)
+	return err
+}
+
+func parse(host string, client *gowebdav.Client, filename string, path string, localDir string, downloadExt []string, strmExt []string) error {
+	fileExt := filepath.Ext(filename)
+	sort.Strings(downloadExt)
+	sort.Strings(strmExt)
+	index := sort.SearchStrings(strmExt, fileExt)
+	if index < len(strmExt) && strmExt[index] == fileExt {
+		return generateSTRM(host, path+"/"+filename, localDir+"/"+path, strings.TrimSuffix(filename, fileExt)+".strm")
+	}
+	if index = sort.SearchStrings(downloadExt, fileExt); index < len(downloadExt) && downloadExt[index] == fileExt {
+		return downloadFile(client, path+"/"+filename, localDir+"/"+path)
+	}
+	return nil
+}
